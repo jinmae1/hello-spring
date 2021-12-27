3bf3bc16b448eb88ac2a0d7ce995442a0d8ccdb2
jinmae1 <jinmae1@gmail.com>
2021-12-27 18:31:05 +0900

    member mvc 및 회원가입 폼 추가

---
+++ src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.kh.spring.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.kh.spring.member.model.service.MemberService;
+import com.kh.spring.member.model.vo.Member;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/member")
+public class MemberController {
+
+	@Autowired
+	private MemberService memberService;
+	
+//	@RequestMapping(value="/memberLogin.do", method=RequestMethod.GET)
+	@GetMapping("/memberLogin.do")
+	public String memberLogin() {
+		return "member/memberLogin";
+	}
+	
+	@GetMapping("/memberEnroll.do")
+	public String memberEnroll() {
+		return "member/memberEnroll";
+	}
+	
+	@PostMapping("/memberEnroll.do")
+	public String memberEnroll(Member member, RedirectAttributes redirectAttr) {
+		log.info("member = {}", member);
+
+		return "redirect:/";
+	}
+	
+	
+}
