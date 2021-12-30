11d52a55a4cf02ab624588dd9c46d400fe34594e
jinmae1 <jinmae1@gmail.com>
2021-12-30 16:12:10 +0900

    선언적 aop -> 프로그래밍적 aop로 변경
    LogAspect처럼 참조하는 방식과 StopWatchAspect처럼 직접하는 방식 두가지 중 택 1

--- src/main/java/com/kh/spring/common/aop/LogAspect.java
+++ src/main/java/com/kh/spring/common/aop/LogAspect.java
@@ -2,12 +2,22 @@ package com.kh.spring.common.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
+@Aspect
 public class LogAspect {
+	
+	@Pointcut("execution(* com.kh.spring.memo..*(..))")
+	public void pointcut() {}
 
+	@Around("pointcut()") // 위에 있는 public void pointcut()을 가리킴
 	public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
 
 		Signature signature = joinPoint.getSignature();	// 메소드 정보
--- src/main/java/com/kh/spring/common/aop/StopWatchAspect.java
+++ src/main/java/com/kh/spring/common/aop/StopWatchAspect.java
@@ -2,14 +2,19 @@ package com.kh.spring.common.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Aspect
+@Component
 public class StopWatchAspect {
 
-
+	@Around("execution(* com.kh.spring.memo.controller.MemoController.insertMemo(..))") // (LogAspect와 대비하여) 이렇게 바로 할 수도 있다.
 	public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
 		
 		Signature signature = joinPoint.getSignature();
