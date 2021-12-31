7f601a63de977eb26a928ed2dadb7524f8d872ba
jinmae1 <jinmae1@gmail.com>
2021-12-31 11:09:52 +0900

    lombok @ToString(callSuper=true) 처리

--- src/main/java/com/kh/spring/board/model/vo/Board.java
+++ src/main/java/com/kh/spring/board/model/vo/Board.java
@@ -5,8 +5,16 @@ import java.util.Date;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+/**
+ * lombok이 super.toString()을 하지 않고 그냥 this.toString()만 해버려서 this의 필드만 toString()하기 때문에 명시적으로 @ToString(callSuper=true) 해준다.
+ * 이 이전 커밋과 이후 커밋을 비교하여 출력결과를 비교해보기
+ * 다시말해 super.toString() + " [attachCount=" + attachCount + "]"; 해줘야 한다는 소리
+ *
+ */
+@ToString(callSuper=true)
 @NoArgsConstructor
 // lombok은 부모 클래스의 @AllArgsConstructor 사용이 불가능하기 때문에 직접 입력해주기
 public class Board extends BoardEntity implements Serializable {
